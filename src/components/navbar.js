6f7d2ebbebdec049cf221a01621b69f48b93c07b
Navi-Beep <estawars30@gmail.com>
2023-09-09 18:07:58 -0500

    changed navbar color

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom'
 export default function Navbar() {
 
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <nav className="navbar navbar-expand-lg" style={{background: "lightblue"}}>
         <div className="container-fluid">
         <a className="navbar-brand" href="/">My App</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
